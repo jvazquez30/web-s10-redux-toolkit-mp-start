89814c371e35c4da3145d4dbd88da5b336f8552f
jvazquez30 <josevazquez881@gmail.com>
2024-05-31 13:24:38 -0700

    Added answers for redux

--- frontend/state/quotesSlice.js
+++ frontend/state/quotesSlice.js
@@ -1,28 +1,74 @@
 // ✨ create your `quotesSlice` in this module
+import { createSlice } from "@reduxjs/toolkit"
+
 
 let id = 1
 const getNextId = () => id++
-const initialState = {
-  displayAllQuotes: true,
-  highlightedQuote: null,
-  quotes: [
-    {
-      id: getNextId(),
-      quoteText: "Don't cry because it's over, smile because it happened.",
-      authorName: "Dr. Seuss",
-      apocryphal: false,
+const slice = createSlice({
+  name: 'quotes',
+  initialState: {
+    displayAllQuotes: true,
+    highlightedQuote: null,
+    quotes: [
+      {
+        id: getNextId(),
+        quoteText: "Don't cry because it's over, smile because it happened.",
+        authorName: "Dr. Seuss",
+        apocryphal: false,
+      },
+      {
+        id: getNextId(),
+        quoteText: "So many books, so little time.",
+        authorName: "Frank Zappa",
+        apocryphal: false,
+      },
+      {
+        id: getNextId(),
+        quoteText: "Be yourself; everyone else is already taken.",
+        authorName: "Oscar Wilde",
+        apocryphal: false,
+      },
+    ],
+  },
+  reducers: {
+    deleteQuote(state, action) {
+      state.quotes = state.quotes
+        .filter(qt => qt.id !== action.payload)
     },
-    {
-      id: getNextId(),
-      quoteText: "So many books, so little time.",
-      authorName: "Frank Zappa",
-      apocryphal: false,
+    highlightQuote(state, action) {
+      if (state.highlightedQuote === action.payload) {
+         state.highlightedQuote = null
+      } else {
+        state.highlightedQuote = action.payload
+      }
     },
-    {
-      id: getNextId(),
-      quoteText: "Be yourself; everyone else is already taken.",
-      authorName: "Oscar Wilde",
-      apocryphal: false,
+    fakeQuote(state, action) {
+      const quote = state.quotes
+        .find(qt => qt.id === action.payload)
+      quote.apocryphal = !quote.apocryphal
+
     },
-  ],
-}
+    addQuote: {
+      prepare(quoteText, authorName,) {
+        return { payload: { id: getNextId(), quoteText, authorName, apocryphal: false } }
+      },
+      reducer(state, action) {
+        state.quotes.push(action.payload)
+      }
+
+    },
+    toggleVisibility(state) {
+      state.displayAllQuotes = !state.displayAllQuotes
+    },
+
+
+
+  }
+
+})
+
+export default slice.reducer
+
+export const {
+  deleteQuote, fakeQuote, highlightQuote, addQuote, toggleVisibility
+} = slice.actions
